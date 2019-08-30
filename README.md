997d8ba9b9f9238f2d1aa062017d7224329e2803
Marina Mosti <marina@mosti.com.mx>
2019-08-30 08:46:28 -0500

    Updates for default dynamic class

--- README.md
+++ README.md
@@ -7,10 +7,10 @@ Ability to target style with `outline-{style}` ie. `outline-solid`
 
 ## Installation
 
+NPM
 `npm install tailwindcss-enhanced-outlines-plugin`
 
-or 
-
+Yarn
 `yarn add tailwindcss-enhanced-outlines-plugin`
 
 Add the following code to your `tailwind.config.js` file
@@ -31,9 +31,12 @@ outlineColor: theme => ({
   ...theme('colors')
 }),
 outlineStyle: {
-  solid: 'solid'
+  default: 'solid',
+  solid: 'solid',
+  dotted: 'dotted'
 },
 outlineWidth: {
+  default: '4px',
   '0': '0',
   '2': '2px',
   '4': '4px',
@@ -42,13 +45,22 @@ outlineWidth: {
 },
 ```
 
+The plugin also exposes a class `outline` that will try to apply the defaults in all 3 configurations, or as they are found.
+
+For example, for this last example.
+`outline` = `outline-width: 4px` + `outline-style: solid`
+
 Now apply the classes to the element you're animating.
-This plugin supports `variants`, and will use the same set as the regular `outline` variants:
+This plugin supports `variants`, add them to your `variants: {}` property.
 
 ```js
 outline: ['responsive', 'focus', 'focus-within'],
 ```
 
+Ex:
+`focus-within: outline-blue`
+`outline-red md:outline-black lg:outline-yellow`
+
 ## Contributions
 
-PRs welcome
+PRs welcome :)
--- index.js
+++ index.js
@@ -2,12 +2,12 @@ module.exports = function ({ addUtilities, theme, variants }) {
   // Flatten fn source: https://github.com/30-seconds/30-seconds-of-code
   const flattenObject = (obj, prefix = '', separator = '-') =>
     Object.keys(obj).reduce((acc, k) => {
-      const pre = prefix.length ? prefix + separator : ''
+      const pre = prefix.length ? prefix + separator : '';
       if (typeof obj[k] === 'object') {
-        Object.assign(acc, flattenObject(obj[k], pre + k))
-      } else acc[pre + k] = obj[k]
-      return acc
-    }, {})
+        Object.assign(acc, flattenObject(obj[k], pre + k));
+      } else acc[pre + k] = obj[k];
+      return acc;
+    }, {});
 
   const widths = theme('outlineWidth', {
     '0': '0',
@@ -15,33 +15,47 @@ module.exports = function ({ addUtilities, theme, variants }) {
     '4': '4px',
     '6': '6px',
     '8': '8px'
-  })
+  });
 
   const styles = theme('outlineStyle', {
     solid: 'solid'
-  })
+  });
 
-  const utils = {}
-  const prefix = '.outline'
+  const colors = flattenObject(theme('outlineColor', {}), '', '-');
+
+  const utils = {};
+  const prefix = '.outline';
+
+  if (widths['default'] || styles['default'] || colors['default']) {
+    utils['.outline'] = {};
+    if (widths['default']) {
+      utils['.outline']['outline-width'] = widths['default'];
+    }
+    if (styles['default']) {
+      utils['.outline']['outline-style'] = styles['default'];
+    }
+    if (colors['default']) {
+      utils['.outline']['outline-color'] = colors['default'];
+    }
+  }
 
   for (let width in widths) {
     utils[`${prefix}-${width}`] = {
       'outline-width': widths[width]
-    }
+    };
   }
 
   for (let style in styles) {
     utils[`${prefix}-${style}`] = {
       'outline-style': styles[style]
-    }
+    };
   }
 
-  const colors = flattenObject(theme('outlineColor', {}), '', '-')
   for (let color in colors) {
     utils[`${prefix}-${color}`] = {
       'outline-color': colors[color]
-    }
+    };
   }
 
-  addUtilities(utils, variants('outline'))
-}
+  addUtilities(utils, variants('outline'));
+};
