2328884de58cb82cfa37843b7d5ecdbbdb34f03d
Marina Mosti <marina@mosti.com.mx>
2020-11-30 12:35:41 +0000

    update: tailwind 2.0 structure

--- index.js
+++ index.js
@@ -1,77 +1,79 @@
-module.exports = function ({ addUtilities, theme, variants }) {
+const plugin = require('tailwindcss/plugin')
+
+module.exports = plugin(function ({ addUtilities, theme, variants }) {
   // Flatten fn source: https://github.com/30-seconds/30-seconds-of-code
   const flattenObject = (obj, prefix = '', separator = '-') =>
     Object.keys(obj).reduce((acc, k) => {
-      const pre = prefix.length ? prefix + separator : '';
+      const pre = prefix.length ? prefix + separator : ''
       if (typeof obj[k] === 'object') {
-        Object.assign(acc, flattenObject(obj[k], pre + k));
-      } else acc[pre + k] = obj[k];
-      return acc;
-    }, {});
+        Object.assign(acc, flattenObject(obj[k], pre + k))
+      } else acc[pre + k] = obj[k]
+      return acc
+    }, {})
 
   const widths = theme('outlineWidth', {
-    '0': '0',
-    '2': '2px',
-    '4': '4px',
-    '6': '6px',
-    '8': '8px'
-  });
+    0: '0',
+    2: '2px',
+    4: '4px',
+    6: '6px',
+    8: '8px'
+  })
 
   const styles = theme('outlineStyle', {
     solid: 'solid'
-  });
+  })
 
-  const colors = flattenObject(theme('outlineColor', {}), '', '-');
+  const colors = flattenObject(theme('outlineColor', {}), '', '-')
 
   const offsets = theme('outlineOffset', {
-    '0': '0',
-    '1': '1px',
-    '2': '2px',
-    '4': '4px'
-  });
+    0: '0',
+    1: '1px',
+    2: '2px',
+    4: '4px'
+  })
 
-  const utils = {};
-  const prefix = '.outline';
+  const utils = {}
+  const prefix = '.outline'
 
-  if (widths['default'] || styles['default'] || colors['default'] || offsets['default']) {
-    utils[prefix] = {};
-    if (widths['default']) {
-      utils[prefix]['outline-width'] = widths['default'];
+  if (widths.DEFAULT || styles.DEFAULT || colors.DEFAULT || offsets.DEFAULT) {
+    utils[prefix] = {}
+    if (widths.DEFAULT) {
+      utils[prefix]['outline-width'] = widths.DEFAULT
     }
-    if (styles['default']) {
-      utils[prefix]['outline-style'] = styles['default'];
+    if (styles.DEFAULT) {
+      utils[prefix]['outline-style'] = styles.DEFAULT
     }
-    if (colors['default']) {
-      utils[prefix]['outline-color'] = colors['default'];
+    if (colors.DEFAULT) {
+      utils[prefix]['outline-color'] = colors.DEFAULT
     }
-    if (offsets['default']) {
-      utils[prefix]['outline-offset'] = offsets['default'];
+    if (offsets.DEFAULT) {
+      utils[prefix]['outline-offset'] = offsets.DEFAULT
     }
   }
 
-  for (let width in widths) {
+  for (const width in widths) {
     utils[`${prefix}-${width}`] = {
       'outline-width': widths[width]
-    };
+    }
   }
 
-  for (let style in styles) {
+  for (const style in styles) {
     utils[`${prefix}-${style}`] = {
       'outline-style': styles[style]
-    };
+    }
   }
 
-  for (let color in colors) {
+  for (const color in colors) {
     utils[`${prefix}-${color}`] = {
       'outline-color': colors[color]
-    };
+    }
   }
 
-  for (let offset in offsets) {
+  for (const offset in offsets) {
     utils[`${prefix}-offset-${offset}`] = {
       'outline-offset': offsets[offset]
-    };
+    }
   }
 
-  addUtilities(utils, variants('outline'));
-};
+  addUtilities(utils, variants('outline'))
+})
